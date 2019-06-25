3f641917c3d76143fa6e7b6976cea7d27b432d77
iunsfa <kevinhuang492@gmail.com>
2019-06-25 00:07:46 -0700

    Checkpoint 2 second commit

--- app/src/main/java/kevinhuang/quizapp/MainActivity.java
+++ app/src/main/java/kevinhuang/quizapp/MainActivity.java
@@ -14,8 +14,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mTextView;
     private Button mTrueButton;
     private Button mFalsebutton;
+    private Button mNextButton;
 
-    private Question firstQuestion;
+    private Question[] mQuestions;
+    private int mIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,32 +26,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalsebutton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
 
         mTrueButton.setOnClickListener(this);
         mFalsebutton.setOnClickListener(this);
+        mNextButton.setOnClickListener(this);
 
         mTextView = (TextView) findViewById(R.id.text_view);
 
-        firstQuestion = new Question(R.string.question_1, true);
-        mTextView.setText(firstQuestion.getTextResId());
+        //Init an array of questions
+        mQuestions = new Question[5];
+        mIndex = 0;
+        //Init each slot of the array
+        mQuestions[0] = new Question(R.string.question_1, true);
+        mQuestions[1] = new Question(R.string.question_2, true);
+        mQuestions[2] = new Question(R.string.question_3, false);
+        mQuestions[3] = new Question(R.string.question_4, true);
+        mQuestions[4] = new Question(R.string.question_5, false);
+
+        //go to whichever question is represented by mIndex
+        mTextView.setText(mQuestions[mIndex].getTextResId());
     }
 
     @Override
     public void onClick(View view) {
 
-        if (view.getId() == R.id.true_button && firstQuestion.getAnswer() == true) {
-            Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
-            myToast.show();
-        }
-        else if (view.getId() == R.id.false_button && firstQuestion.getAnswer() == false) {
-            Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
-            myToast.show();
-        }
-        else{
-            Toast myToast = Toast.makeText(this, "You are incorrect", Toast.LENGTH_SHORT);
-            myToast.show();
-        }
+        if (view.getId() == R.id.true_button) {
+            checkAnswer(true);
+        } else if (view.getId() == R.id.false_button ) {
+            checkAnswer(false);
+        } else if (view.getId() == R.id.next_button) {
+            //change to the next question
+            //increment the index by 1
+            mIndex++;
 
+            //do if statement here
+            // if the mIndex is greater than equal 
+            //change text in view
+            mTextView.setText(mQuestions[mIndex].getTextResId());
+
+        }
+    }
+        public boolean checkAnswer(boolean userInput){
+            if(mQuestions[mIndex].getAnswer() == userInput){
+                Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
+                myToast.show();
+                return true;
+            }
+            else{
+                Toast myToast = Toast.makeText(this, "You are incorrect", Toast.LENGTH_SHORT);
+                myToast.show();
+                return false;
+            }
 
 
     }
