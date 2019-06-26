58d5af35f04dd2542547ad6eef818b62a455f599
iunsfa <kevinhuang492@gmail.com>
2019-06-25 23:44:52 -0700

    Checkpoint 2 3rd commit

--- app/src/main/java/kevinhuang/quizapp/MainActivity.java
+++ app/src/main/java/kevinhuang/quizapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,10 +15,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mTextView;
     private Button mTrueButton;
     private Button mFalsebutton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+
 
     private Question[] mQuestions;
     private int mIndex;
+    public int mScore;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +32,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalsebutton = (Button) findViewById(R.id.false_button);
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
 
         mTrueButton.setOnClickListener(this);
         mFalsebutton.setOnClickListener(this);
         mNextButton.setOnClickListener(this);
+        mPrevButton.setOnClickListener(this);
 
         mTextView = (TextView) findViewById(R.id.text_view);
+
+
 
         //Init an array of questions
         mQuestions = new Question[5];
         mIndex = 0;
+        mScore = 0;
+
         //Init each slot of the array
-        mQuestions[0] = new Question(R.string.question_1, true);
-        mQuestions[1] = new Question(R.string.question_2, true);
-        mQuestions[2] = new Question(R.string.question_3, false);
-        mQuestions[3] = new Question(R.string.question_4, true);
-        mQuestions[4] = new Question(R.string.question_5, false);
+        mQuestions[0] = new Question(R.string.question_1, true,R.string.hint_1);
+        mQuestions[1] = new Question(R.string.question_2, true,R.string.hint_2);
+        mQuestions[2] = new Question(R.string.question_3, false,R.string.hint_3);
+        mQuestions[3] = new Question(R.string.question_4, true,R.string.hint_4);
+        mQuestions[4] = new Question(R.string.question_5, false,R.string.hint_5);
 
         //go to whichever question is represented by mIndex
         mTextView.setText(mQuestions[mIndex].getTextResId());
@@ -53,32 +65,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if (view.getId() == R.id.true_button) {
             checkAnswer(true);
-        } else if (view.getId() == R.id.false_button ) {
+        }
+
+        else if (view.getId() == R.id.false_button ) {
             checkAnswer(false);
-        } else if (view.getId() == R.id.next_button) {
+        }
+
+        else if(view.getId() == R.id.prev_button){
+            mIndex--;
+
+
+
+        }
+
+        else if (view.getId() == R.id.next_button) {
             //change to the next question
             //increment the index by 1
             mIndex++;
 
+
+
             //do if statement here
             // if the mIndex is greater than equal 
             //change text in view
-            mTextView.setText(mQuestions[mIndex].getTextResId());
+
+
+
 
         }
+        else if (view.getId() == R.id.hint_button){
+            Toast myToast2 = Toast.makeText(this,mQuestions[mIndex].getmHintTextResId(), Toast.LENGTH_LONG);
+            myToast2.show();
+        }
+        if(mIndex>4 || mIndex<0){
+            mIndex = 0;
+        }
+        mTextView.setText(mQuestions[mIndex].getTextResId());
+
+
     }
         public boolean checkAnswer(boolean userInput){
             if(mQuestions[mIndex].getAnswer() == userInput){
+                mScore++;
                 Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
                 myToast.show();
                 return true;
+
+
             }
             else{
+                mScore--;
                 Toast myToast = Toast.makeText(this, "You are incorrect", Toast.LENGTH_SHORT);
                 myToast.show();
                 return false;
+
             }
 
 
     }
+
 }
--- app/src/main/java/kevinhuang/quizapp/Question.java
+++ app/src/main/java/kevinhuang/quizapp/Question.java
@@ -3,10 +3,12 @@ package kevinhuang.quizapp;
 public class Question {
     private int mTextResId;
     private boolean mAnswer;
+    private int mHintTextResId;
 
-    public Question(int textResId, boolean answer) {
+    public Question(int textResId, boolean answer, int hintTextResId) {
         mTextResId = textResId;
         mAnswer = answer;
+        mHintTextResId = hintTextResId;
     }
 
     public int getTextResId() {
@@ -23,5 +25,9 @@ public class Question {
 
     public void setAnswer(boolean answer) {
         mAnswer = answer;
+    }
+
+    public int getmHintTextResId() {
+        return mHintTextResId;
     }
 }
